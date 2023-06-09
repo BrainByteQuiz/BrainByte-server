f1c60ba0fdcc9210218ed10ec893663f38ac792e
Nitj'sefni <75166987+Nitjsefnie@users.noreply.github.com>
2023-06-09 15:03:40 +0000

    feat: add delete user

---
+++ src/repositories/user/delete.ts
@@ -0,0 +1,20 @@
+import { Result } from "@badrap/result";
+import { createHash } from 'crypto';
+import { findUser } from "../common";
+import { User } from "@prisma/client";
+import prisma from "../client";
+
+const deleteUser = async (data: { id: string}): Promise<Result<User>> => {
+  try {
+    const user = await prisma.user.findFirst({where: { id: data.id, deletedAt: null }});
+    if (!user) {
+        throw new Error("User does not exist");
+    }
+    const result = await prisma.user.update({where: { id: data.id }, data: { deletedAt: Date()}});
+    return Result.ok(result);
+  } catch (e) {
+    return Result.err(e as Error);
+  }
+};
+
+export default deleteUser;
