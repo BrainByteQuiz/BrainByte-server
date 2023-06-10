a93c56b38b04c620f8ea60b767550d8de267baab
David Štorek <xstorek@fi.muni.cz>
2023-06-10 22:48:01 +0200

    feat: Rework quiz create query

--- src/repositories/quiz/create.ts
+++ src/repositories/quiz/create.ts
@@ -1,15 +1,51 @@
 import { Result } from "@badrap/result";
 import client from '../client';
 import type { QuizCreateData } from "./types/data";
-import type { QuizCreateResult } from "./types/result";
+import type { GenericQuestionResult, QuizCreateResult } from "./types/result";
+import { Answer } from "@prisma/client";
 
 const create = async (data: QuizCreateData): QuizCreateResult => {
   try {
     return Result.ok(
-      await client.quiz.create({
-        data: {
-          ...data
-        },
+      await client.$transaction(async (tx) => {
+        const quiz = await tx.quiz.create({
+          data: {
+            ...data.data,
+          },
+        });
+
+        const questions: GenericQuestionResult[] = [];
+        for (let q = 0; q < data.questions.length; ++q) {
+          const question = data.questions[q];
+          const questionData = await tx.question.create({
+            data: {
+              ...question.data,
+              quizId: question.quizId,
+            },
+          });
+
+          const answers: Answer[] = [];
+          for (let a = 0; a < question.answers.length; ++a) {
+            const answer = question.answers[a];
+            const answerData = await tx.answer.create({
+              data: {
+                ...answer,
+              },
+            });
+
+            answers.push(answerData);
+          }
+
+          questions.push({
+            ...questionData,
+            answers,
+          });
+        }
+
+        return {
+          ...quiz,
+          questions,
+        };
       }),
     );
   } catch (e) {
