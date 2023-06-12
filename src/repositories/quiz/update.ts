67b41608b6291dff12bf015e50fe4139a45cd840
David Štorek <d.storek@seznam.cz>
2023-06-12 15:47:50 +0200

    chore: Fix formatting

--- src/repositories/quiz/update.ts
+++ src/repositories/quiz/update.ts
@@ -6,28 +6,27 @@ import client from "../client";
 const update = async (data: QuizUpdateData): QuizUpdateResult => {
   try {
     return Result.ok(await client.$transaction(async (tx) => {
-        const quiz = await tx.quiz.findUniqueOrThrow({
-          where: {
-            id: data.id,
-          },
-        });
+      const quiz = await tx.quiz.findUniqueOrThrow({
+        where: {
+          id: data.id,
+        },
+      });
 
-        if (quiz.creatorId !== data.creatorId) {
-          throw new Error("The user is not the author of this quiz");
-        }
+      if (quiz.creatorId !== data.creatorId) {
+        throw new Error("The user is not the author of this quiz");
+      }
 
-        const result = await tx.quiz.update({
-          where: {
-            id: data.id,
-          },
-          data: {
-            ...data.data,
-          },
-        });
+      const result = await tx.quiz.update({
+        where: {
+          id: data.id,
+        },
+        data: {
+          ...data.data,
+        },
+      });
 
-        return result;
-      })
-    );
+      return result;
+    }));
   } catch (e) {
     return Result.err(e as Error);
   }
