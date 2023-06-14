09815540c653d81321ab8b67a84aec02ba53cc8c
David Štorek <d.storek@seznam.cz>
2023-06-14 15:20:32 +0200

    feat: Add quiz create controller

---
+++ src/controllers/quiz/create.ts
@@ -0,0 +1,27 @@
+import create from "../../repositories/quiz/create";
+import type { Request, Response } from "express";
+import { errorResponse } from "../common";
+
+const createQuizControl = async (req: Request, res: Response) => {
+    if (!req.params['name'] || !req.params['creatorId']) {
+        res.status(400).send(errorResponse("Required fields are missing"));
+    }
+
+    const result = await create({
+        name: req.params['name'],
+        description: req.params['description'],
+        creatorId: req.params['creatorId'],
+        questions: req.params['questions'],
+    });
+    if (result.isErr) {
+        res.status(404).send(errorResponse(result.error.message));
+    }
+
+    return res.status(200).send({
+        status: 'success',
+        data: result.unwrap(),
+        message: '',
+    });
+};
+
+export default createQuizControl;
